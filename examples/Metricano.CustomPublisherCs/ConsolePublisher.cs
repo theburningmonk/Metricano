fcb5a8dac954a1e620a4fe1b230a592fb9305920
theburningmonk <theburningmonk@gmail.com>
2014-08-25 21:31:09 +0100

    - added custom publisher example in C# - closes #1

---
+++ examples/Metricano.CustomPublisherCs/ConsolePublisher.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Metricano.CustomPublisherCs
+{
+    public class ConsolePublisher : IMetricsPublisher
+    {
+        public async Task Publish(Metric[] metrics)
+        {
+            foreach (var metric in metrics)
+            {
+                Publish(metric);
+            }
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("Bye bye world...");
+        }
+
+        private static TimeSpan GetNinetyFivePercentile(TimeSpan[] timeSpans)
+        {
+            var skipCount = timeSpans.Length * 0.05;
+            return timeSpans
+                    .OrderByDescending(ts => ts.TotalMilliseconds)
+                     .Skip((int)skipCount)
+                     .FirstOrDefault();
+        }
+
+        private static void Publish(Metric metric)
+        {
+            if (metric.IsCount)
+            {
+                var countMetric = (Metric.Count)metric;
+                Console.WriteLine(
+                    @"Count metric ({0}):
+Max             : {1}
+Min             : {2}
+Average         : {3}
+Total           : {4}
+Sample Size     : {5}",
+                    countMetric.Name,
+                    countMetric.Max,
+                    countMetric.Min,
+                    countMetric.Average,
+                    countMetric.Sum,
+                    countMetric.SampleCount);
+            }
+            else
+            {
+                var timeMetric = (Metric.TimeSpan)metric;
+                var rawTimes = timeMetric.Item.Raw;
+                Console.WriteLine(
+                    @"TimeSpan metric ({0}):
+Max             : {1}ms
+Min             : {2}ms
+Average         : {3}ms
+Total           : {4}ms
+Sample Size     : {5}
+95 percentile   : {6}ms",
+                    timeMetric.Name,
+                    timeMetric.Max,
+                    timeMetric.Min,
+                    timeMetric.Average,
+                    timeMetric.Sum,
+                    timeMetric.SampleCount,
+                    GetNinetyFivePercentile(rawTimes).TotalMilliseconds);
+
+            }
+        }
+    }
+}
---
+++ examples/Metricano.CustomPublisherCs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+using Metricano.PostSharpAspects;
+
+namespace Metricano.CustomPublisherCs
+{
+    /// <summary>
+    /// Let this run for couple of minutes to generate data points in CloudWatch.
+    /// Press Ctrl+C or close the console window to stop it.
+    /// </summary>
+    [CountExecution]    // multi-cast to all methods, public & private
+    [LogExecutionTime]  // multi-cast to all methods, public & private
+    public class Program
+    {
+        private static readonly Random Rand = new Random((int)DateTime.UtcNow.Ticks);
+
+        public static void Main(string[] args)
+        {
+            Publish.With(new ConsolePublisher());
+
+            while (true)
+            {
+                TakesRandomTimeToExecute();
+            }
+        }
+
+        private static void TakesRandomTimeToExecute()
+        {
+            Thread.Sleep(Rand.Next(42));
+        }
+    }
+}
